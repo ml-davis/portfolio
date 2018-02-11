0efdb2d826d9e12857221a951a6339d5d5178ebf
matthew <ml.davis195@gmail.com>
2018-02-10 19:31:06 -0500

    Added basic navbar using Angular Material tabs.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule, MatButtonModule, MatCardModule, MatTabsModule } from '@angular/material';
 import { NgModule } from '@angular/core';
-
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-
+import { AboutMeComponent } from './about-me/about-me.component';
+import { WorkHistoryComponent } from './work-history/work-history.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PersonalProjectsComponent } from './personal-projects/personal-projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutMeComponent,
+    WorkHistoryComponent,
+    NavbarComponent,
+    PersonalProjectsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatCardModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
